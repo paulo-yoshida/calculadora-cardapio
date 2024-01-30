b1daa0e6bbf2d3d947c99a8e947ee43570faa813
paulo-yoshida <152620326+paulo-yoshida@users.noreply.github.com>
2024-01-30 17:30:48 -0300

    initial commit

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+// Import dependencies
+const fs = require("fs");
+const csv = require('csvtojson');
+
+(async () => {
+  // load csv
+  const tbalimentos = await csv().fromFile("alimentos.csv");
+  
+  //
+  console.log(tbalimentos);
+})();
+
