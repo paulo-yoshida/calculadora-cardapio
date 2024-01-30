b079a4c7c1b9e51a266a4358cf6adce775940891
paulo-yoshida <152620326+paulo-yoshida@users.noreply.github.com>
2024-01-30 18:13:51 -0300

    atualiza js

--- calculadora.js
+++ calculadora.js
@@ -1,12 +1,16 @@
-// Import dependencies
-const fs = require("fs");
-const csv = require('csvtojson');
+const url = 'https://raw.githubusercontent.com/paulo-yoshida/calculadora-cardapio/b1daa0e6bbf2d3d947c99a8e947ee43570faa813/alimentos.csv';
 
-(async () => {
-    // load csv
-  const tbalimentos = await csv().fromFile("alimentos.csv");
-  
-  //
-  console.log(tbalimentos);
-})();
-
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Não foi possível carregar o arquivo CSV.');
+    }
+    return response.text();
+  })
+  .then(csvData => {
+    // Processar os dados do CSV
+    console.log(csvData);
+  })
+  .catch(error => {
+    console.error('Erro:', error);
+  });
